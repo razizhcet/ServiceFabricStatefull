1fa73f0df9b3733dff5be444ee224d814a005a26
Razi Ahmad <razizhcet@gmail.com>
2018-04-16 20:25:01 +0530

    first commit

---
+++ SimpleStoreApplication/ShoppingCartService/ShoppingCartService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.ServiceModel;
+using Common;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Runtime;
+using Microsoft.ServiceFabric.Services.Communication.Wcf;
+
+namespace ShoppingCartService
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ShoppingCartService : StatefulService, IShoppingCartService
+    {
+        public ShoppingCartService(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        private NetTcpBinding CreateListenBinding()
+        {
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None)
+            {
+                SendTimeout = TimeSpan.MaxValue,
+                ReceiveTimeout = TimeSpan.MaxValue,
+                OpenTimeout = TimeSpan.FromSeconds(5),
+                CloseTimeout = TimeSpan.FromSeconds(5),
+                MaxConnections = int.MaxValue,
+                MaxReceivedMessageSize = 1024 * 1024
+            };
+            binding.MaxBufferSize = (int)binding.MaxReceivedMessageSize;
+            binding.MaxBufferPoolSize = Environment.ProcessorCount * binding.MaxReceivedMessageSize;
+            return binding;
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        public IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[]
+            {
+                new ServiceInstanceListener(context =>
+                    new WcfCommunicationListener<IShoppingCartService>(
+                        wcfServiceObject:this,
+                        serviceContext:context,
+                        endpointResourceName: "ServiceEndpoint",
+                        listenerBinding: WcfUtility.CreateTcpListenerBinding()
+                    )
+            )};
+        }
+
+        public async Task AddItem(ShoppingCartItem item)
+        {
+            var cart = await this.StateManager.GetOrAddAsync<IReliableDictionary<string,
+                ShoppingCartItem>>("myCart");
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                await cart.AddOrUpdateAsync(tx, item.ProductName, item, (k, v) => item);
+                await tx.CommitAsync();
+            }
+        }
+
+
+        public async Task DeleteItem(string productName)
+        {
+            var cart = await this.StateManager.GetOrAddAsync<IReliableDictionary<string,
+                ShoppingCartItem>>("myCart");
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var existing = await cart.TryGetValueAsync(tx, productName);
+                if (existing.HasValue) await cart.TryRemoveAsync(tx, productName);
+                await tx.CommitAsync();
+            }
+        }
+
+
+        public async Task<List<ShoppingCartItem>> GetItems()
+        {
+            var cart = await this.StateManager.GetOrAddAsync<IReliableDictionary<string,
+                ShoppingCartItem>>("myCart");
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var ret = from t in cart
+                          select t.Value;
+                return ret.ToList();
+            }
+        }
+
+    }
+}
---
+++ SimpleStoreApplication/SimpleStoreClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.ServiceModel;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+using System.Fabric;
+using Common;
+
+namespace SimpleStoreClient
+{
+    internal static class Program
+    {
+        private static NetTcpBinding CreateClientConnectionBinding()
+        {
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None)
+            {
+                SendTimeout = TimeSpan.MaxValue,
+                ReceiveTimeout = TimeSpan.MaxValue,
+                OpenTimeout = TimeSpan.FromSeconds(5),
+                CloseTimeout = TimeSpan.FromSeconds(5),
+                MaxReceivedMessageSize = 1024 * 1024
+            };
+            binding.MaxBufferSize = (int)binding.MaxReceivedMessageSize;
+            binding.MaxBufferPoolSize = Environment.ProcessorCount * binding.MaxReceivedMessageSize;
+
+            return binding;
+        }
+
+
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Uri ServiceName = new Uri("fabric:/SimpleStoreApplication/ShoppingCartService");
+            ServicePartitionResolver serviceResolver = new ServicePartitionResolver(() =>
+            new FabricClient());
+            NetTcpBinding binding = CreateClientConnectionBinding();
+            Client shoppingClient = new Client(new WcfCommunicationClientFactory<IShoppingCartService>(serviceResolver, binding, null), ServiceName);
+            shoppingClient.AddItem(new ShoppingCartItem
+            {
+                ProductName = "XBOX ONE",
+                UnitPrice = 329.0, Amount = 2 }).Wait();
+            var list = shoppingClient.GetItems().Result;
+            foreach (var item in list)
+            {
+                Console.WriteLine(string.Format("{0}: {1:C2} X {2} = {3:C2}",
+                    item.ProductName, item.UnitPrice, item.Amount, item.LineTotal));
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
